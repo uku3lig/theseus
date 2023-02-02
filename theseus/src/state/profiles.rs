56eb3fc50958a5522caace14606b949cd2bcc212
uku <uku3lig@gmail.com>
2023-02-02 19:48:49 +0100

    make profiles even more public

--- theseus/src/state/profiles.rs
+++ theseus/src/state/profiles.rs
@@ -12,7 +12,7 @@ use tokio::fs;
 const PROFILE_JSON_PATH: &str = "profile.json";
 const PROFILE_SUBTREE: &[u8] = b"profiles";
 
-pub(crate) struct Profiles(pub HashMap<PathBuf, Option<Profile>>);
+pub struct Profiles(pub HashMap<PathBuf, Option<Profile>>);
 
 // TODO: possibly add defaults to some of these values
 pub const CURRENT_FORMAT_VERSION: u32 = 1;
